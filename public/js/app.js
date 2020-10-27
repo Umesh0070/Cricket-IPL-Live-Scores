4058735320fdc1c4c76f7b74e0f60cbb5fa89838
Umesh <umeshstatus2@gmail.com>
2020-10-27 18:43:09 +0530

    added logos and modified condition to display them

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 console.log("Client side javascript file is loaded!");
-var uniqueId = "1216520";
-var uid = 1216520;
+var uniqueId = "1216524";
 const url =
   "https://cricapi.com/api/cricketScore?apikey=0AJFDtoMeaWUVHi4LZTsRingmcM2&unique_id=" +
   uniqueId;
@@ -16,7 +15,7 @@ const team1Wick = document.querySelector("#team1-wik");
 const team2 = document.querySelector("#team2");
 const team2Runs = document.querySelector("#team2-runs");
 const team2Wick = document.querySelector("#team2-wik");
-
+const updatedAt = document.querySelector("#updated");
 var current = null;
 var previous = null;
 
@@ -31,18 +30,32 @@ var timer = setInterval(() => {
       console.log(data);
       if (data.error) {
         console.log(data.error);
+        clearInterval(timer);
       } else if (data.matchStarted) {
         status.textContent = "Live Matches";
         stats.textContent = data.stat;
         // description.textContent = data.description
         team1.textContent = data["team-1"] + ":";
         team2.textContent = "  " + data["team-2"] + ":";
+        data["team-1"].includes("Hyderabad") || data["team-2"].includes("Hyderabad") ? document.getElementById("team1-logo").src = "assets/images/srh_logo.png":"false"
+        data["team-1"].includes("Delhi") || data["team-2"].includes("Delhi")? document.getElementById("team2-logo").src = "assets/images/dc_logo.png":"false"
+        data["team-1"].includes("Mumbai") || data["team-2"].includes("Mumbai") ? document.getElementById("team1-logo").src = "assets/images/mi_logo-.png":"false"
+        data["team-1"].includes("Bangalore") || data["team-2"].includes("Bangalore") ? document.getElementById("team2-logo").src = "assets/images/rcb_logo-.png":"false"
+        data["team-1"].includes("Chennai") || data["team-2"].includes("Chennai") ? document.getElementById("team1-logo").src = "assets/images/csk_logo-.png":"false"
+        data["team-1"].includes("Kolkata") || data["team-2"].includes("Kolkata") ? document.getElementById("team2-logo").src = "assets/images/kkr_logo-.png":"false"
+        data["team-1"].includes("Punjab") || data["team-2"].includes("Punjab") ? document.getElementById("team1-logo").src = "assets/images/kxip_logo-.png":"false"
+        data["team-1"].includes("Rajasthan") || data["team-2"].includes("Rajasthan") ? document.getElementById("team2-logo").src = "assets/images/rr_logo-.png":"false"
+
         let onlyScore = data.score.match(/[0-9]+/g);
         if (onlyScore[0]) {
           team1Runs.textContent = onlyScore[0];
+        }else{
+            team1Runs.textContent = "Loading.."
         }
         if (onlyScore[1]) {
           team1Wick.textContent = "/" + onlyScore[1];
+        }else {
+            team1Wick.textContent = "/0";
         }
         if (onlyScore[2]) {
           team2Runs.textContent = onlyScore[2];
@@ -50,27 +63,38 @@ var timer = setInterval(() => {
         if (onlyScore[3]) {
           team2Wick.textContent = "/" + onlyScore[3];
         }
+        if (!onlyScore[2] || !onlyScore[3]){
+            team2Runs.textContent == "Yet to Bat"
+        }
 
-        // score.textContent = data.score
-        // console.log(team1Runs,team1Wickets)
-        // current = team1Runs
         if (previous && current && previous !== current) {
           console.log("refresh");
           window.location.reload();
         }
         previous = current;
       } else if (!data.matchStarted) {
-        status.textContent = "Upcoming Matches";
-        description.textContent = data.description;
+        status.textContent = "Upcoming";
+        team1.textContent = data["team-1"] + " Vs" +"  " + data["team-2"]
+        updateDate = new Date(data.provider["pubDate"]);
+        updatedAt.textContent = "Last Updated at " +  updateDate.getHours() +":" +updateDate.getMinutes()+" Hrs" 
+        // team2.textContent = "  " + data["team-2"] + ":";
+        // description.textContent = data.description;
+        data["team-1"].includes("Hyderabad") || data["team-2"].includes("Hyderabad") ? document.getElementById("team1-logo").src = "assets/images/srh_logo.png":"false"
+        data["team-1"].includes("Delhi") || data["team-2"].includes("Delhi")? document.getElementById("team2-logo").src = "assets/images/dc_logo.png":"false"
+        data["team-1"].includes("Mumbai") || data["team-2"].includes("Mumbai") ? document.getElementById("team1-logo").src = "assets/images/mi_logo-.png":"false"
+        data["team-1"].includes("Bangalore") || data["team-2"].includes("Bangalore") ? document.getElementById("team2-logo").src = "assets/images/rcb_logo-.png":"false"
+        data["team-1"].includes("Chennai") || data["team-2"].includes("Chennai") ? document.getElementById("team1-logo").src = "assets/images/csk_logo-.png":"false"
+        data["team-1"].includes("Kolkata") || data["team-2"].includes("Kolkata") ? document.getElementById("team2-logo").src = "assets/images/kkr_logo-.png":"false"
+        data["team-1"].includes("Punjab") || data["team-2"].includes("Punjab") ? document.getElementById("team1-logo").src = "assets/images/kxip_logo-.png":"false"
+        data["team-1"].includes("Rajasthan") || data["team-2"].includes("Rajasthan") ? document.getElementById("team2-logo").src = "assets/images/rr_logo-.png":"false"
 
         fetch(matchUrl).then((response) => {
           response.json().then((data) => {
             let allMatches = data["matches"];
-            let reqMatch = allMatches.find((x) => x.unique_id === uid);
+            let reqMatch = allMatches.find((x) => x.unique_id === parseInt(uniqueId));
             date = new Date(reqMatch.dateTimeGMT);
             stats.textContent =
-              "Match starts at " + date.getHours() + ":" + 30 + " Hrs";
-
+              "Match starts at " + date.getHours() + ":" + date.getMinutes() + " IST Stay Tuned for Live Scores !";
             // console.log(date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate() + '-'+date.getHours()+":"+30)
           });
         });
