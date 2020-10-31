976cdf04b20a2b2acc2db0f8786feb28c74ceb11
Umesh <umeshstatus2@gmail.com>
2020-10-31 22:16:51 +0530

    Removed un-necessary files and code

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const myPort = process.env.PORT || 3000
-const cric = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../views')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const myPort = process.env.PORT;
+const cric = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../views");
 
-cric.set('views', viewsPath)
-cric.set('view engine','hbs')
-cric.use(express.static(publicDirectoryPath))
+cric.set("views", viewsPath);
+cric.set("view engine", "hbs");
+cric.use(express.static(publicDirectoryPath));
 
-cric.get('',(req,res) => {
-    res.render('index')
-})
+cric.get("", (req, res) => {
+  res.render("index");
+});
 
-cric.listen(myPort,() => {
-    console.log("Server is up on " + myPort)
-})
+cric.listen(myPort, () => {
+  console.log("Server is up on " + myPort);
+});
